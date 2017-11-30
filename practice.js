65ffeda7a927a64df0aeb05ea4d0de1f84a83a6c
Ye Wang <wangye.tju@gmail.com>
2017-11-29 19:48:40 -0500

    Add angular js to practice 5

--- practice.js
+++ practice.js
@@ -76,7 +76,41 @@ searchApp.controller('srchBrCtrl', function($scope){
 	]
 });
 
+//Switchable Grid
+var switchableApp = angular.module('switchableGrid',[]);
+switchableApp.controller('SGCtrl', function($scope){
+    $scope.items = [
+        {
+            title: '50 Must-have plugins for extending Twitter Bootstrap',
+            image: 'images/Board-Games.png'
+        },
+        {
+            title: 'Making a Super Simple Registration System With PHP and MySQL',
+            image: 'images/cloud-2044823_960_720.png'
+        },
+        {
+            title: 'Create a slide-out footer with this neat z-index trick',
+            image: 'images/home-1110868_960_720.png'
+        },
+        {
+            title: 'How to Make a Digital Clock with jQuery and CSS3',
+            image: 'images/Mobile-Smartphone-icon.png'
+        },
+        {
+            title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
+            image: 'images/parts-1086747_640.png'
+        },
+        {
+            title: 'Your First Backbone.js App – Service Chooser',
+            image: 'images/Puzzle.png'
+        },
+    ]
+
+    $scope.layout = 'grid';
+});
+
 //Bootstrap multiple apps
 angular.bootstrap(document.getElementById("inlineEditor"), ['inlEdt']);
 angular.bootstrap(document.getElementById("priceForm"), ['prcFrm']);
 angular.bootstrap(document.getElementById("searchBar"), ['srchBr']);
+angular.bootstrap(document.getElementById('GLSwitch'), ['switchableGrid']);
